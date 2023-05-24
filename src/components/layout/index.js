dca97646bffeff7b761f3a8d542f9663cba0a455
habid22 <habid22@uwo.ca>
2023-05-24 13:57:52 -0400

    f

---
+++ src/components/layout/index.js
@@ -0,0 +1,30 @@
+
+import { useEffect } from 'react';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom'
+import { LOGIN } from '../../lib/routers';
+import {useAuth} from "../../hooks/auth";
+
+export default function Layout() {
+ const {pathname} = useLocation();
+ const navigate = useNavigate();
+ const {user, isLoading} = useAuth();
+
+
+
+ useEffect(() => {
+    if (pathname.startsWith("/protected") && !user) {
+        navigate(LOGIN);
+    }
+
+ }, [pathname, user])
+
+ if(isLoading) return "Loading..."
+
+ return (
+    <>
+
+   
+    
+    </>
+ );
+}
