3413e06251724d98a07671a23f35e10d07f3e73a
habid22 <habid22@uwo.ca>
2023-05-30 12:38:39 -0400

    f

--- src/hooks/auth.js
+++ src/hooks/auth.js
@@ -1,10 +1,12 @@
 import { useAuthState, useSignOut } from 'react-firebase-hooks/auth';
-import { auth } from "../lib/firebase";
+import { auth, db } from "../lib/firebase";
 import { useState } from "react";
 import { DASHBOARD, LOGIN } from '../lib/routers';
-import { signInWithEmailAndPassword } from 'firebase/auth';
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
 import { useToast } from '@chakra-ui/react';
+import { set } from 'react-hook-form';
+import { setDoc, doc } from 'firebase/firestore';
 
 export function useAuth() {
 
@@ -37,7 +39,7 @@ export function useLogin() {
         catch (error){
             toast ({
                 title: "Log in failed",
-                description: error.message,
+                description: "Make sure you've entered the correct credentials!",
                 status: "error",
                 isClosable: true,
                 position: "top",
@@ -50,6 +52,66 @@ export function useLogin() {
         return true // Return true if login success
     }
     return {login, isLoading}
+}
+
+export function useRegister() {
+    const [isLoading, setLoading] = useState(false);
+    const toast = useToast();
+    async function register({username, email, password, redirectTo = DASHBOARD})
+
+    {
+        setLoading(true);
+       const usernameExists = await isUsernameExists(username)
+
+       if(usernameExists) {
+        toast({
+            title: "Username already exists",
+            status: "error",
+            isClosable: true,
+            position: "top",
+            duration: 5000,
+        });
+        setLoading(false);
+  
+       }
+       else {
+        try {
+            const res = await createUserWithEmailAndPassword(auth, email, password);
+
+            await setDoc(doc(db, "users", res.user.uid), {
+                id: res.user.uid, username: username.toLowerCase(), avatar: "", date: Date.now(),
+            })
+
+            toast({
+                title: "Account created",
+                description: "You are now logged in",
+                status: "success",
+                isClosable: true,
+                position: "top",
+                duration: 5000,
+            });
+
+            navigate(redirectTo);
+        }
+        catch (error) {
+            toast({
+                title: "Account creation failed",
+                description: error.message,
+                status: "error",
+                isClosable: true,
+                position: "top",
+                duration: 5000,
+            });
+
+        } finally {
+            setLoading(false);
+        }
+
+       setLoading(false);
+
+    
+
+    return {register, isLoading};
 }
 
 export function useLogout() {
