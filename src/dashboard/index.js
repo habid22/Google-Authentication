41274227645430074816c0fa763b27754e4da07a
habid22 <habid22@uwo.ca>
2023-06-21 12:37:06 -0400

    3

--- src/dashboard/index.js
+++ src/dashboard/index.js
@@ -1,9 +1,51 @@
+import { HStack, Heading, Textarea } from '@chakra-ui/react'
 import React from 'react'
+import { Box } from '@chakra-ui/react'
+import { Button } from '@chakra-ui/react'
+import TextareaAutosize from 'react-textarea-autosize';
+import { useForm } from 'react-hook-form';
+import { useAddPost } from '../hooks/posts';
+import { useAuth } from '../hooks/auth';
 
 export default function Dashbaord() {
+  const {register, handleSubmit, reset} = useForm();
+  const {addPost, isLoading: addingPost} = useAddPost();  
+  const {user, isLoading: authLoading} = useAuth();
+
+  function handleAddPost(data) {
+    addPost({
+      uid: user.id,
+      text: data.text,
+
+    })
+    console.log(data);
+    reset();
+  }
+
   return (
-    <div>
+  <Box maxW="600" mx="auto"  py="10">
+    <form onSubmit={handleSubmit(handleAddPost)}>
+      <HStack justify="space-between">
+        <Heading size="lg">New Post</Heading>
+        <Button colorScheme='teal' type="submit" isLoading={authLoading || addingPost}
+        loadingText="Loading"> Post </Button>
+      </HStack>
+      <Textarea a
+        s={TextareaAutosize}
+        resize ="none" 
+        mt="5" 
+        placeholder="Create a new post..."
+        minRows={3}
+        {...register("text", {required: true})} 
+        />
+
+
       
-    </div>
-  )
+    </form>
+
+
+
+  </Box>
+   
+  );
 }
