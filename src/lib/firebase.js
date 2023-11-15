3d7d2ad3a09356cfcfe9428b51ffb15c290ac730
habid22 <habid22@uwo.ca>
2023-11-14 20:36:23 -0500

    Update Firebase configuration

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -5,14 +5,16 @@ import {getAuth} from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCpYbEo0pGz9Nxl0_1HJfex91VIHvW_ms0",
-  authDomain: "authentication-trial2.firebaseapp.com",
-  projectId: "authentication-trial2",
-  storageBucket: "authentication-trial2.appspot.com",
-  messagingSenderId: "610407552838",
-  appId: "1:610407552838:web:22d0a8046377964a5c7747",
-  measurementId: "G-MPD03M0MG0"
+  apiKey: "AIzaSyDVn4Iuh5I753wI2Ecjx-D7RgJWDNyak5U",
+  authDomain: "authentication-3-3de54.firebaseapp.com",
+  projectId: "authentication-3-3de54",
+  storageBucket: "authentication-3-3de54.appspot.com",
+  messagingSenderId: "969776544650",
+  appId: "1:969776544650:web:8e7dcac2d3635c8d9b2673",
+  measurementId: "G-P2MH0030QV"
 };
 
 // Initialize Firebase
