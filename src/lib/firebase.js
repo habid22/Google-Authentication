2e577036238a210fc72801b6fed84783a2b9cafb
habid22 <habid22@uwo.ca>
2023-11-14 20:12:48 -0500

    Add yarn package manager to dependencies and update Firebase configuration

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,13 +3,16 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import {getAuth} from "firebase/auth";
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD3gYCv2Vng75v0QsvAkwDZRPQnuWEPCes",
-  authDomain: "authentication-page-309dd.firebaseapp.com",
-  projectId: "authentication-page-309dd",
-  storageBucket: "authentication-page-309dd.appspot.com",
-  messagingSenderId: "388022773014",
-  appId: "1:388022773014:web:9ebcac77a699b0a6a51c37"
+  apiKey: "AIzaSyCpYbEo0pGz9Nxl0_1HJfex91VIHvW_ms0",
+  authDomain: "authentication-trial2.firebaseapp.com",
+  projectId: "authentication-trial2",
+  storageBucket: "authentication-trial2.appspot.com",
+  messagingSenderId: "610407552838",
+  appId: "1:610407552838:web:22d0a8046377964a5c7747",
+  measurementId: "G-MPD03M0MG0"
 };
 
 // Initialize Firebase
